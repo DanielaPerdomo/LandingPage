1ad27035266c407f34d3589cfb2fe8be439ed187
Screin208 <snay208@gmail.com>
2023-06-22 00:31:32 +0000

    algo

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -6,13 +6,13 @@ const Cards = () => {
       <h2 className="fs-1" style={{ marginTop: "35px" }}>
         <strong>Portfolio</strong>
       </h2>
-      <p className="fs-4">
+      <p className="fs-3">
         Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula
         porta felis euismod semper. Praesent commodo cursus magna, vel
         scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
       </p>
       <div className="row">
-        <div className="col-sm-4">
+        <div className="col-sm-3">
           <div className="card">
             <img
               src="https://picsum.photos/200/300"
@@ -28,7 +28,7 @@ const Cards = () => {
             </div>
           </div>
         </div>
-        <div className="col-sm-4">
+        <div className="col-sm-3">
           <div className="card">
             <img
               src="https://picsum.photos/200/300"
@@ -44,7 +44,7 @@ const Cards = () => {
             </div>
           </div>
         </div>
-        <div className="col-sm-4">
+        <div className="col-sm-3">
           <div className="card">
             <img
               src="https://picsum.photos/200/300"
@@ -55,6 +55,22 @@ const Cards = () => {
               <h4 className="card-title text-primary">Project Name</h4>
               <p className="card-text">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dignissimos neque eos, deleniti pariatur harum quia.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-3">
+          <div className="card">
+            <img
+              src="https://picsum.photos/200/300"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h4 className="card-title text-primary">Project Name</h4>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Dignissimos neque eos, deleniti pariatur harum quia.
               </p>
             </div>
