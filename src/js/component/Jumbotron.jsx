93c6fcf758e52f6356adf57e5a4541c16d706fcb
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-06-22 00:52:54 +0000

    Navbar y Jumbotron

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Jumbotron = () => {
-  return "Jumbotron";
+  return (
+    <div>
+      <h1>A Warm Welcome</h1>
+      <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Call to Action!</a>
+    </div>
+  );
 };
 export default Jumbotron;
