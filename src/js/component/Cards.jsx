38a3eebcc6900eee625a1983cb882cdc3c1e81bd
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-06-23 02:33:13 +0000

    Edicion de los componentes en sus archivos

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -2,77 +2,72 @@ import React from "react";
 
 const Cards = () => {
   return (
-    <div className="container border-top mx-auto px-5 mb-5">
-      <h2 className="fs-1" style={{ marginTop: "35px" }}>
-        <strong>Portfolio</strong>
-      </h2>
-      <p className="fs-3">
-        Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula
-        porta felis euismod semper. Praesent commodo cursus magna, vel
-        scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
-      </p>
+    <div className="container mx-auto px-5 mb-5">
       <div className="row">
         <div className="col-sm-3">
           <div className="card">
             <img
-              src="https://picsum.photos/200/300"
+              src="https://picsum.photos/500/325"
               className="card-img-top"
-              alt="..."
-            />
+              alt="..."/>
             <div className="card-body">
-              <h4 className="card-title text-primary">Project Name</h4>
+              <h4 className="card-title text-black">Card title</h4>
               <p className="card-text">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Dignissimos neque eos, deleniti pariatur harum quia.
               </p>
+              <a href="#" class="btn btn-primary">Find Out More!</a>
             </div>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="card">
             <img
-              src="https://picsum.photos/200/300"
+              src="https://picsum.photos/500/325"
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
-              <h4 className="card-title text-primary">Project Name</h4>
+              <h4 className="card-title text-black">Card title</h4>
               <p className="card-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Dignissimos neque eos, deleniti pariatur harum quia.
               </p>
+              <a href="#" class="btn btn-primary">Find Out More!</a>
             </div>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="card">
             <img
-              src="https://picsum.photos/200/300"
+              src="https://picsum.photos/500/325"
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
-              <h4 className="card-title text-primary">Project Name</h4>
+              <h4 className="card-title text-black">Card title</h4>
               <p className="card-text">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Dignissimos neque eos, deleniti pariatur harum quia.
               </p>
+              <a href="#" class="btn btn-primary">Find Out More!</a>
             </div>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="card">
             <img
-              src="https://picsum.photos/200/300"
+              src="https://picsum.photos/500/325"
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
-              <h4 className="card-title text-primary">Project Name</h4>
+              <h4 className="card-title text-black">Card title</h4>
               <p className="card-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Dignissimos neque eos, deleniti pariatur harum quia.
               </p>
+              <a href="#" class="btn btn-primary">Find Out More!</a>
             </div>
           </div>
         </div>
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer>
+      <footer className="bg-dark text-light">
         <div className="row">
           <div className="col-lg-12">
-            <p>Copyright &copy; Your Website Name 2021</p>
+            <p className="mb-0 fw-lighter">Copyright &copy; Elaborado por ♠ Daniela Perdomo & Ruben Reyes ♠</p>
           </div>
         </div>
       </footer>
